449c8a24d630b80c88373e8b41845e4bc32f2d66
golu <abhinavatpluto@gmail.com>
2025-01-11 22:43:31 +0530

    Intersection of array

--- LeetCodeSolutions/src/main/java/org/example/solution/Solution.java
+++ LeetCodeSolutions/src/main/java/org/example/solution/Solution.java
@@ -1,2 +1,38 @@
-package org.example.solution;public class Solution {
+package org.example.solution;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+    public static int[] interArr(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> intersection = new ArrayList<>();
+
+        // Count frequencies of elements in nums1
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // Find intersections with nums2
+        for (int num : nums2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                intersection.add(num);
+                map.put(num, map.get(num) - 1); // Decrease count
+            }
+        }
+
+        // Convert list to array
+        int[] result = new int[intersection.size()];
+        for (int i = 0; i < intersection.size(); i++) {
+            result[i] = intersection.get(i);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
